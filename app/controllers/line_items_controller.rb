840c7e109009d534d394df9b45a4f1ff7d748b9c
Anna Krawczyk <anna.krapek@gmail.com>
2012-12-22 10:45:26 +0100

    Adding possibility  to destroy individual line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -74,12 +74,14 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1
   # DELETE /line_items/1.json
+
+
   def destroy
     @line_item = LineItem.find(params[:id])
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to current_cart }
       format.json { head :no_content }
     end
   end
